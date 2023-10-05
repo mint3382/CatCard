b2288ba020116349906338504817e084abc6705f
SooJi <sokoooool86@gmail.com>
2023-10-05 19:48:07 +0900

    :lipstick: Design: 액션 버튼 생성

---
+++ CatCard/CatCard/CardActionButton.swift
@@ -0,0 +1,21 @@
+//
+//  CardActionButton.swift
+//  CatCard
+//
+//  Created by TORI on 2023/10/05.
+//
+
+import UIKit
+
+final class CardActionButton: UIButton {
+    
+    // MARK: - Initialization
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+}

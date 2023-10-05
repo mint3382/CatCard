ad5944c7f0967f37d321b428126d8e2c395685c4
SooJi <sokoooool86@gmail.com>
2023-10-05 21:22:09 +0900

    :sparkles: Feat: 카드 선택시 애니메이션 효과 추가

--- CatCard/CatCard/CardView.swift
+++ CatCard/CatCard/CardView.swift
@@ -9,6 +9,14 @@ import UIKit
 
 final class CardView: UIView {
     
+    // MARK: - Properties
+    
+    private var isSelected: Bool = false {
+        didSet {
+            isSelected ? zoomInCard() : zoomOutCard()
+        }
+    }
+    
     // MARK: - Views
     
     private let iconImageView: UIImageView = {
@@ -42,12 +50,14 @@ final class CardView: UIView {
     override init(frame: CGRect) {
         super.init(frame: frame)
         configureStyle()
+        configureGesture()
         setupLayout()
     }
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
         configureStyle()
+        configureGesture()
         setupLayout()
     }
     
@@ -69,6 +79,12 @@ final class CardView: UIView {
         layer.borderColor = UIColor.systemPink.cgColor
     }
     
+    private func configureGesture() {
+        let gesture = UITapGestureRecognizer()
+        gesture.addTarget(self, action: #selector(tapAnimation))
+        addGestureRecognizer(gesture)
+    }
+    
     // MARK: - Constraints
     
     private func setupLayout() {
@@ -80,5 +96,26 @@ final class CardView: UIView {
             contentStack.leadingAnchor.constraint(equalTo: layoutMarginsGuide.leadingAnchor),
             contentStack.trailingAnchor.constraint(equalTo: layoutMarginsGuide.trailingAnchor),
         ])
+    }
+}
+
+extension CardView {
+    
+    // MARK: - Actions
+    
+    @objc private func tapAnimation() {
+        isSelected.toggle()
+    }
+    
+    private func zoomInCard() {
+        UIView.animate(withDuration: 0.1) {
+            self.transform = .init(scaleX: 1.1, y: 1.1)
+        }
+    }
+    
+    private func zoomOutCard() {
+        UIView.animate(withDuration: 0.1) {
+            self.transform = .identity
+        }
     }
 }

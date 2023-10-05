55db39f86ed95b58a2af9c5a30f7ad70c52c1b64
mint3382 <mskim0926@naver.com>
2023-10-05 23:58:28 +0900

    🔧chore : 파일 위치 변경

--- CatCard/CatCard/Card/CardDummy.swift
+++ CatCard/CatCard/Card/CardDummy.swift
@@ -7,46 +7,46 @@
 
 import Foundation
 
-struct User: Player {
-    var resourceCards: [ResourceType : Int] = [.love: 0, .food: 0, .toy: 0, .time: 0, .money: 0]
-    var catCards: [CatSpecies : Int] = [.cat: 0, .tiger: 0]
-}
-
-struct Computer: Player {
-    var resourceCards: [ResourceType : Int] = [.love: 0, .food: 0, .toy: 0, .time: 0, .money: 0]
-    var catCards: [CatSpecies : Int] = [.cat: 0, .tiger: 0]
-}
-
-
 final class CardDummy {
-    var catDummy: [CatSpecies: Int] = [.cat: 16, .tiger: 1]
+    var catDummy: [CatSpecies: Int] = [.cat: 16, .tiger: 2]
     var resourceDummy: [ResourceType: Int] = [.love: 70, .food: 50, .toy: 40, .time: 30, .money: 10]
     
+    //시작할때 12장 랜덤 카드 배부
     func distributeStartCards(player: inout Player) {
         for _ in 1...12 {
             randomResource(player: &player)
         }
     }
     
+    //랜덤한 카드 뽑기
     func randomResource(player: inout Player) {
         guard let pick = resourceDummy.randomElement() else {
             return
         }
         
-        resourceDummy[pick.key] = pick.value - 1
-        player.addResourceCard(resourceType: pick.key)
-        
+        if pick.value != 0 {
+            resourceDummy[pick.key] = pick.value - 1
+            player.addResourceCard(resourceType: pick.key)
+        } else {
+            randomResource(player: &player)
+        }
     }
     
-    func changeResourceToCatCard(player: inout Player) {
+    //랜덤한 고양이 카드 1장 뽑기
+    func randomCatCard(player: inout Player) {
         guard let pick = catDummy.randomElement() else {
             return
         }
         
-        catDummy[pick.key] = pick.value - 1
-        player.addRandomCatSpeciesCard(catSpecies: pick.key)
+        if pick.value != 0 {
+            catDummy[pick.key] = pick.value - 1
+            player.addCatCard(catSpecies: pick.key)
+        } else {
+            randomCatCard(player: &player)
+        }
     }
     
+    //자원카드 교환하기
     func exchangeResourceCard(player: inout Player, cards: [ResourceType: Int]) {
         var allCount: Int = 0
         for (resourceType, count) in cards {
@@ -60,5 +60,14 @@ final class CardDummy {
         for _ in 1...allCount {
             randomResource(player: &player)
         }
+    }
+    
+    //호랑이 카드 개수 확인
+    func CountingTigerCard() -> Int {
+        guard let tigerCard = catDummy[.tiger] else {
+            return 0
+        }
+        
+        return tigerCard
     }
 }

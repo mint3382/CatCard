f10f6eb2fc3303f9c283741d0c0af1464ee5b577
SooJi <sokoooool86@gmail.com>
2023-10-05 20:44:13 +0900

    :sparkles: Feat: 카드 아이템 모델 생성

---
+++ CatCard/CatCard/CardItem.swift
@@ -0,0 +1,14 @@
+//
+//  CardItem.swift
+//  CatCard
+//
+//  Created by TORI on 2023/10/05.
+//
+
+import UIKit
+
+struct CardItem {
+    let imageString: String
+    let number: Int
+    let color: UIColor
+}

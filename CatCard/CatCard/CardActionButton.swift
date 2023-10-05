9b15e77ae52299fab9a4c11f6bd70e3e00590a49
SooJi <sokoooool86@gmail.com>
2023-10-05 19:48:49 +0900

    :lipstick: Design: 액션 버튼 스타일 설정 추가

--- CatCard/CatCard/CardActionButton.swift
+++ CatCard/CatCard/CardActionButton.swift
@@ -13,9 +13,29 @@ final class CardActionButton: UIButton {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
+        configureButton()
     }
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
+        configureButton()
+    }
+    
+    // MARK: - Configure View
+    
+    private func configureButton() {
+        setTitleColor(.black, for: .normal)
+        titleLabel?.font = .systemFont(ofSize: 16, weight: .bold)
+        backgroundColor = .systemGreen
+        clipsToBounds = true
+        layer.cornerRadius = 10
+        layer.borderWidth = 3
+        layer.borderColor = UIColor.black.cgColor
+        
+        layer.masksToBounds = false
+        layer.shadowColor = UIColor.black.cgColor
+        layer.shadowRadius = 0.1
+        layer.shadowOpacity = 1.0
+        layer.shadowOffset = .init(width: 4.0, height: 4.0)
     }
 }

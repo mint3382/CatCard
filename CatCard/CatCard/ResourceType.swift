dd313011512077ef9a57f96f622b2dd20c4b0af5
mint3382 <mskim0926@naver.com>
2023-10-05 21:26:56 +0900

    ✨feat: ResourceType 추가

---
+++ CatCard/CatCard/ResourceType.swift
@@ -0,0 +1,16 @@
+//
+//  ResourceCard.swift
+//  CatCard
+//
+//  Created by minsong kim on 2023/10/05.
+//
+
+import Foundation
+
+enum ResourceType: CaseIterable {
+    case love
+    case food
+    case toy
+    case time
+    case money
+}

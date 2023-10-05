ebc63056a357daef6a35258ae67bf0419d923678
SooJi <sokoooool86@gmail.com>
2023-10-06 00:31:12 +0900

    :lipstick: Design: 카드 선택 뷰 내부 기본 레이아웃 설정

--- CatCard/CatCard/CardPickerView.swift
+++ CatCard/CatCard/CardPickerView.swift
@@ -9,11 +9,39 @@ import UIKit
 
 final class CardPickerView: UIView {
     
+    private lazy var actionButtonStack: UIStackView = {
+        let stack = UIStackView()
+        return stack
+    }()
+    
+    private lazy var cardStack: UIStackView = {
+        let stack = UIStackView()
+        return stack
+    }()
+    
+    private lazy var contentStack: UIStackView = {
+        let stack = UIStackView()
+        return stack
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
+        setupLayout()
     }
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
+        setupLayout()
+    }
+    
+    private func setupLayout() {
+        addSubview(contentStack)
+        
+        NSLayoutConstraint.activate([
+            contentStack.topAnchor.constraint(equalTo: layoutMarginsGuide.topAnchor),
+            contentStack.bottomAnchor.constraint(equalTo: layoutMarginsGuide.bottomAnchor),
+            contentStack.leadingAnchor.constraint(equalTo: layoutMarginsGuide.leadingAnchor),
+            contentStack.trailingAnchor.constraint(equalTo: layoutMarginsGuide.trailingAnchor),
+        ])
     }
 }

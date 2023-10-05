7dceabffb2fec583621d506a1af53b08c394a51b
SooJi <sokoooool86@gmail.com>
2023-10-05 22:09:55 +0900

    :lipstick: Design: 캐릭터를 나타내는 컨텐츠 생성

--- CatCard/CatCard/CharacterView.swift
+++ CatCard/CatCard/CharacterView.swift
@@ -9,6 +9,40 @@ import UIKit
 
 final class CharacterView: UIView {
     
+    // MARK: - Views
+    
+    private let charaterImageView: UIImageView = {
+        let view = UIImageView()
+        view.image = UIImage(systemName: "person.fill")
+        return view
+    }()
+    
+    private let characterNameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "빡빡이"
+        label.font = .systemFont(ofSize: 16, weight: .bold)
+        return label
+    }()
+    
+    private let turnStateLabel: UILabel = {
+        let label = UILabel()
+        label.text = "차례"
+        label.font = .systemFont(ofSize: 16, weight: .bold)
+        label.isEnabled = false
+        return label
+    }()
+    
+    private lazy var contentStack: UIStackView = {
+        let stack = UIStackView(arrangedSubviews: [charaterImageView,
+                                                  characterNameLabel,
+                                                  turnStateLabel])
+        stack.axis = .vertical
+        stack.alignment = .center
+        stack.distribution = .fill
+        stack.spacing = 8
+        return stack
+    }()
+    
     // MARK: - Initialization
     
     override init(frame: CGRect) {

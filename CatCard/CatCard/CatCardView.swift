a49079bec8ee0b837b8bbd613140346ac0405680
SooJi <sokoooool86@gmail.com>
2023-10-06 09:59:51 +0900

    :lipstick: Design: 캣카드뷰 내부 이미지 레이아웃 설정 추가

--- CatCard/CatCard/CatCardView.swift
+++ CatCard/CatCard/CatCardView.swift
@@ -9,11 +9,40 @@ import UIKit
 
 final class CatCardView: UIView {
     
+    private let hiddenCardView: UIImageView = {
+        let view = UIImageView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    private let openCardView: UIImageView = {
+        let view = UIImageView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
     }
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
+    }
+    
+    private func setupLayout() {
+        addSubview(hiddenCardView)
+        addSubview(openCardView)
+        
+        NSLayoutConstraint.activate([
+            hiddenCardView.widthAnchor.constraint(equalTo: widthAnchor, multiplier: 0.5),
+            hiddenCardView.heightAnchor.constraint(equalTo: heightAnchor),
+            hiddenCardView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            hiddenCardView.centerYAnchor.constraint(equalTo: centerYAnchor),
+            
+            openCardView.widthAnchor.constraint(equalTo: widthAnchor, multiplier: 0.5),
+            openCardView.heightAnchor.constraint(equalTo: heightAnchor),
+            openCardView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            openCardView.centerYAnchor.constraint(equalTo: centerYAnchor),
+        ])
     }
 }
